83f87193ac62e325a29fb21848d957805f50faeb
kinshusharm0412 <kinshusharm0412@gmail.com>
2024-04-04 16:33:07 +0530

    speed se push kiya jyada koi kaam nahi hai

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ if False:
 	url_page = st_javascript("await fetch('').then(r => window.parent.location.href)",key=str(x))
 	x+=1
 	if url_page!=0:
-		break
+		pass#break
 	else:
 		st.write(url_page)
 		time.sleep(1)
