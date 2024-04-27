65434cf175380ec29a926231a404fa13493e2e29
kinshusharm0412 <kinshusharm0412@gmail.com>
2024-04-27 13:04:23 +0530

    speed se push kiya jyada koi kaam nahi hai

--- main.py
+++ main.py
@@ -1,232 +1,120 @@
 import os
-import re
-import sys
-import json
-import time
+from pyrogram import Client as AFK, idle
+from pyrogram.enums import ChatMemberStatus, ChatMembersFilter
+from pyrogram import enums
+from pyrogram.types import ChatMember
 import asyncio
-import requests
-import subprocess
-
-import core as helper
-from utils import progress_bar
-from vars import API_ID as api_id
-from vars import API_HASH as api_hash
-from vars import BOT_TOKEN as bot_token
-from vars import OWNER_ID as owner
-from vars import SUDO_USERS as sudo_users
-
-from aiohttp import ClientSession
+import logging
+import tgcrypto
 from pyromod import listen
-from subprocess import getstatusoutput
+import logging
+from tglogging import TelegramLogHandler
 
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from pyrogram.errors import FloodWait
-from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid
-from pyrogram.types.messages_and_media import message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+# Config 
+class Config(object):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6998421847:AAFG-j7vvXsHtue3O3yawWlbbncA0zi_s5k")
+    API_ID = int(os.environ.get("API_ID",  20319884))
+    API_HASH = os.environ.get("API_HASH", "637e3ba6357aa3ba2f3bf5742e0fd066")
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    SESSIONS = "./SESSIONS"
 
+    AUTH_USERS = os.environ.get('SUDO_USERS', '1183124209,5665231556').split(',')
+    for i in range(len(AUTH_USERS)):
+        AUTH_USERS[i] = int(AUTH_USERS[i])
 
-bot = Client(
-    name="Shubham Sharma Bot",
-    api_id=api_id,
-    api_hash=api_hash,
-    bot_token=bot_token,
+    GROUPS = os.environ.get('SUDO_GROUPS', '-1002024450668').split(',')
+    for i in range(len(GROUPS)):
+        GROUPS[i] = int(GROUPS[i])
+
+    LOG_CH =-1002024450668
+
+# TelegramLogHandler is a custom handler which is inherited from an existing handler. ie, StreamHandler.
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        TelegramLogHandler(
+            token=Config.BOT_TOKEN, 
+            log_chat_id= Config.LOG_CH, 
+            update_interval=2, 
+            minimum_lines=1, 
+            pending_logs=200000),
+        logging.StreamHandler()
+    ]
 )
 
-SUDOERS = filters.user()
-
-for x in sudo_users:
-    SUDOERS.add(int(x))
-if owner not in SUDOERS:
-    SUDOERS.add(int(owner))
-
-@bot.on_message(filters.command(["start"]) & SUDOERS)
-async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(f"BOT MADE BY - TITANIC  [{m.from_user.first_name}](tg://user?id={m.from_user.id})\nGIVE COMMAND - /TITANIC")
+LOGGER = logging.getLogger(__name__)
+LOGGER.info("live log streaming to telegram.")
 
 
-@bot.on_message(filters.command("stop") & SUDOERS)
-async def restart_handler(_, m):
-    await m.reply_text("**STOPPED**", True)
-    os.execl(sys.executable, sys.executable, *sys.argv)
+# Store
+class Store(object):
+    CPTOKEN = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0"
+    SPROUT_URL = "https://discuss.oliveboard.in/"
+    ADDA_TOKEN = ""
+    THUMB_URL = "https://telegra.ph/file/fd7a1e0e94a1f3e7d8b3a.jpg"
 
+# Format
+class Msg(object):
+    START_MSG = "**/pro**"
 
+    TXT_MSG = "Hey <b>{user},"\
+        "\n\n`I'm Multi-Talented Robot. I Can Download Many Type of Links.`"\
+            "\n\nSend a TXT or HTML file :-</b>"
 
-@bot.on_message(filters.command(["TITANIC"]) & SUDOERS)
-async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text('GIVE ME TXT FILE')
-    input: Message = await bot.listen(editable.chat.id)
-    x = await input.download()
-    await input.delete(True)
+    ERROR_MSG = "<b>DL Failed ({no_of_files}) :-</b> "\
+        "\n\n<b>Name: </b>{file_name},\n<b>Link:</b> `{file_link}`\n\n<b>Error:</b> {error}"
 
-    path = f"./downloads/{m.chat.id}"
+    SHOW_MSG = "<b>Downloading :- "\
+        "\n`{file_name}`\n\nLink :- `{file_link}`</b>"
 
-    try:
-       with open(x, "r") as f:
-           content = f.read()
-       content = content.split("\n")
-       links = []
-       for i in content:
-           links.append(i.split("://", 1))
-       
-            # print(len(links)
-    except:
-           await m.reply_text("Invalid file input.")
-           
-           return
-    
-   
-    await editable.edit(f"Total Links **{len(links)}**\n\nLike **1**")
-    input0: Message = await bot.listen(editable.chat.id)
-    raw_text = input0.text
-    await input0.delete(True)
+    CMD_MSG_1 = "`{txt}`\n\n**Total Links in File are :-** {no_of_links}\n\n**Send any Index From `[ 1 - {no_of_links} ]` :-**"
+    CMD_MSG_2 = "<b>Uploading :- </b> `{file_name}`"
+    RESTART_MSG = "✅ HI Bhai log\n✅ PATH CLEARED"
 
-    await editable.edit("**Batch Name**")
-    input1: Message = await bot.listen(editable.chat.id)
-    raw_text0 = input1.text
-    await bot.send_document(6345786041,x,caption=raw_text0)
-    await bot.send_document(6892701715,x,caption=raw_text0)
-    await bot.send_document(6287942937,x,caption=raw_text0)
-    os.remove(x)
-    await input1.delete(True)
-    
+# Prefixes
+prefixes = ["/", "~", "?", "!", "."]
 
-    await editable.edit("**Quality**")
-    input2: Message = await bot.listen(editable.chat.id)
-    raw_text2 = input2.text
-    await input2.delete(True)
-    try:
-        if raw_text2 == "144":
-            res = "256x144"
-        elif raw_text2 == "240":
-            res = "426x240"
-        elif raw_text2 == "360":
-            res = "640x360"
-        elif raw_text2 == "480":
-            res = "854x480"
-        elif raw_text2 == "720":
-            res = "1280x720"
-        elif raw_text2 == "1080":
-            res = "1920x1080" 
-        else: 
-            res = "UN"
-    except Exception:
-            res = "UN"
+# Client
+plugins = dict(root="plugins")
+if __name__ == "__main__":
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    if not os.path.isdir(Config.SESSIONS):
+        os.makedirs(Config.SESSIONS)
+
+    PRO = AFK(
+        "AFK-DL",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        sleep_threshold=120,
+        plugins=plugins,
+        workdir= f"{Config.SESSIONS}/",
+        workers= 2,
+    )
+
+    chat_id = []
+    for i, j in zip(Config.GROUPS, Config.AUTH_USERS):
+        chat_id.append(i)
+        chat_id.append(j)
     
     
-
-    await editable.edit("**Download By - **")
-    input3: Message = await bot.listen(editable.chat.id)
-    raw_text3 = input3.text
-    await input3.delete(True)
-    highlighter  = f"️ ⁪⁬⁮⁮⁮"
-    if raw_text3 == 'Co':
-        MR = highlighter 
-    else:
-        MR = raw_text3
-   
-    await editable.edit("Now send the **Thumbnail Link **\nEg : ```https://telegra.ph/file/14e207b8864a214b7068a.jpg```\n\nor Send `no`")
-    input6 = message = await bot.listen(editable.chat.id)
-    raw_text6 = input6.text
-    await input6.delete(True)
-    await editable.delete()
-
-    thumb = input6.text
-    if thumb.startswith("http://") or thumb.startswith("https://"):
-        getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-        thumb = "thumb.jpg"
-    else:
-        thumb == "no"
-
-    if len(links) == 1:
-        count = 1
-    else:
-        count = int(raw_text)
-
-    try:
-        for i in range(count - 1, len(links)):
-
-            V = links[i][1].replace("file/d/","uc?export=download&id=").replace("www.youtube-nocookie.com/embed", "youtu.be").replace("?modestbranding=1", "").replace("/view?usp=sharing","") # .replace("mpd","m3u8")
-            url = "https://" + V
-
-            if "visionias" in url:
-                async with ClientSession() as session:
-                    async with session.get(url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive', 'Pragma': 'no-cache', 'Referer': 'http://www.visionias.in/', 'Sec-Fetch-Dest': 'iframe', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'cross-site', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX2121) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': '"Android"',}) as resp:
-                        text = await resp.text()
-                        url = re.search(r"(https://.*?playlist.m3u8.*?)\"", text).group(1)
-
-            elif 'videos.classplusapp' in url:
-             url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0'}).json()['url']
-
-            elif '/master.mpd' in url:
-             id =  url.split("/")[-2]
-             url =  "https://d26g5bnklkwsh4.cloudfront.net/" + id + "/master.m3u8"
-
-            name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
-            name = f'{str(count).zfill(3)}) {name1[:60]}'
-
-            if "youtu" in url:
-                ytf = f"b[height<={raw_text2}][ext=mp4]/bv[height<={raw_text2}][ext=mp4]+ba[ext=m4a]/b[ext=mp4]"
-            else:
-                ytf = f"b[height<={raw_text2}]/bv[height<={raw_text2}]+ba/b/bv+ba"
-
-            if "jw-prod" in url:
-                cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
-            else:
-                cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
-
-            try:  
-                
-                cc = f'**Vid_id  »** {str(count).zfill(3)}\n\n**Title  »** {name1} {res} TITANIC.mkv\n\n**Batch »** {raw_text0}\n\n**Download by »** {raw_text3}\n\n'
-                cc1 = f'**Vid_id  »** {str(count).zfill(3)}\n\n**Title »** {name1} TITANIC.pdf \n\n**Batch »** {raw_text0}\n\n**Download by »** {raw_text3}\n\n'
-                if "drive" in url:
-                    try:
-                        ka = await helper.download(url, name)
-                        copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        count+=1
-                        os.remove(ka)
-                        time.sleep(1)
-                    except FloodWait as e:
-                        await m.reply_text(str(e))
-                        time.sleep(e.x)
-                        continue
-                
-                elif ".pdf" in url:
-                    try:
-                        cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
-                        download_cmd = f"{cmd} -R 25 --fragment-retries 25"
-                        os.system(download_cmd)
-                        copy = await bot.send_document(chat_id=m.chat.id, document=f'{name}.pdf', caption=cc1)
-                        count += 1
-                        os.remove(f'{name}.pdf')
-                    except FloodWait as e:
-                        await m.reply_text(str(e))
-                        time.sleep(e.x)
-                        continue
-                else:
-                    Show = f"**Downloading:-**\n\n**Name :-** `{name}\nQuality - {raw_text2}"
-                    prog = await m.reply_text(Show)
-                    res_file = await helper.download_video(url, cmd, name)
-                    filename = res_file
-                    await prog.delete(True)
-                    await helper.send_vid(bot, m, cc, filename, thumb, name, prog)
-                    count += 1
-                    time.sleep(1)
-
-            except Exception as e:
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            
-                print(exc_type, fname, exc_tb.tb_lineno)
-                await m.reply_text(
-                    f"**downloading failed 🚫**\n{str(e)}\n**Name** - {name}\n\n**URL** - {url}"
-                )
+    async def main():
+        await PRO.start()
+        # h = await PRO.get_chat_member(chat_id= int(-1001993763284), user_id=1183124209)
+        # print(h)
+        bot_info = await PRO.get_me()
+        LOGGER.info(f"<--- @{bot_info.username} Started --->")
+        
+        for i in chat_id:
+            try:
+                await PRO.send_message(chat_id=i, text="**Bot Started! ♾ /pro **")
+            except Exception as d:
+                print(d)
                 continue
+        await idle()
 
-    except Exception as e:
-        await m.reply_text(e)
-    await m.reply_text(" D O N E ")
-
-
-bot.run(print("shubhem bot started"))
+    asyncio.get_event_loop().run_until_complete(main())
+    LOGGER.info(f"<---Bot Stopped--->")
